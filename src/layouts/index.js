78510382b9f2bb735ee6ad59d0c0b5fd69b73b15
crosssenses <lari_wu@gmx.de>
2019-04-08 12:11:43 +0200

    Integrity check for CDN scripts not working › removed from script tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -34,9 +34,9 @@ const TemplateWrapper = ({ children }) => {
         <meta name="msapplication-config" content="favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <script type="application/javascript" data-main="https://widget.supr.com/app/main" src="https://widget.supr.com/load.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" />
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" />
+        <script type="application/javascript" data-main="https://widget.supr.com/app/main" src="https://widget.supr.com/load.js" />
 
         <script src="scripts/js.cookie.js" type="text/javascript"></script>
         <script src="scripts/panke.js"></script>
