9250146debf91d765a1ebdf1cec021ef22697540
crosssenses <lari_wu@gmx.de>
2019-04-11 15:07:17 +0200

    Improve off-canvas navi

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,13 @@ import Navigation from '../components/navigation'
 export default () => (
         <header>
           <div className="row">
-            <div className="col-md-4 col-xs-6">
+            <div className="col-md-4 col-9">
                 <a href="/" title="Go to Homepage"><p className="logotype">panke.gallery</p></a>
             </div>
             <div className="col-md-8 text-right d-none d-sm-block">
               <Navigation />
             </div>
-            <div className="col-xs-6 text-right d-block d-sm-none">
+            <div className="col-3 text-right d-block d-sm-none">
               <button className="toggle-menu">
                 <i className="fas fa-bars"></i>
               </button>
