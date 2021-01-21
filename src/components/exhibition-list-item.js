121e5948d2ecdff3a1d3178ab40c84c53938bde1
crosssenses <lari_wu@gmx.de>
2021-01-21 17:37:53 +0100

    date to be confirmed on exhibition template

--- src/components/exhibition-list-item.js
+++ src/components/exhibition-list-item.js
@@ -8,6 +8,9 @@ class exhibitionListItem  extends React.Component{
   render(){
 
     const exhibition = this.props.exhibition
+
+    // --- Date or to be confirmed --
+
     let dateDisplayed
 
     if (exhibition.dateTbc){
@@ -16,6 +19,8 @@ class exhibitionListItem  extends React.Component{
     else{
       dateDisplayed = Moment(exhibition.startDate).format('DD MMMM') + '\u2009\u2013\u2009' + Moment(exhibition.endDate).format('DD MMMM YYYY')
     }
+
+    // --- Output --
 
     return(
       <article className="exhibition-item">
@@ -35,17 +40,3 @@ class exhibitionListItem  extends React.Component{
 }
 
 export default exhibitionListItem
-//export default ({ exhibition }) => (
-//  <article className="exhibition-item">
-//    <Link to={`/exhibition/${exhibition.slug}`}>
-//      <Img alt="FeaturedImage" sizes={{...exhibition.featuredImage.sizes , aspectRatio: 16/9}} />
-//    </Link>
-//    <h3>
-//      <Link to={`/exhibition/${exhibition.slug}`}>{exhibition.title}</Link>
-//      <small>{Moment(exhibition.startDate).format('DD MMMM')}&thinsp;&ndash;&thinsp;{Moment(exhibition.endDate).format('DD MMMM YYYY')}</small>
-//    </h3>
-//    <div dangerouslySetInnerHTML={{
-//        __html: exhibition.subtitleShortDescription.childMarkdownRemark.html
-//      }} />
-//  </article>
-//)
