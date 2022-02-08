9542dff440ecd6414f2b5ab3ed8330fdc2582e6d
crosssenses <lari_wu@gmx.de>
2022-02-08 10:57:58 +0100

    debug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 let contentfulConfig;
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 try {
   // Load the Contentful config from the .contentful.json
   contentfulConfig = require('./.contentful')
--- src/components/youtubePlaylist.js
+++ src/components/youtubePlaylist.js
@@ -18,7 +18,7 @@ class YoutubePlaylist extends React.Component{
     const res = await fetch(`${YOUTUBE_PLAYLIST_API}?part=snippet&maxResults=50&playlistId=${playlist_id}&key=${YOUTUBE_API_KEY}`)
     const data = await res.json();
 
-    console.log('data: ', data);
+//    console.log('data: ', data);
 
     this.setState({
       playlistData: data
