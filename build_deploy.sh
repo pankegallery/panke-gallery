b4e68ecf24834cc8eab2f4f45bc85c3555d1da56
crosssenses <lari_wu@gmx.de>
2022-06-23 11:46:26 +0200

    rosa shows events from the series rosa and build script optimisation

---
+++ build_deploy.sh
@@ -0,0 +1,24 @@
+# Delete cache and build
+rm -fr ./.cache && gatsby build
+
+# Ask for deploy
+read -p "Deploy preview? " -n 1 -r deploy_yes
+echo    # (optional) move to a new line
+if [[ $deploy_yes =~ ^[Yy]$ ]]
+then
+  # Deploy preview
+  netlify deploy
+  
+  # Aks for live
+  read -p "Go live? " -n 1 -r live_yes
+  echo    # (optional) move to a new line
+  if [[ $live_yes =~ ^[Yy]$ ]]
+  then
+    # Deploy preview
+    netlify deploy --prod
+  fi
+# Otherwise serve locally
+else
+  gatsby serve
+fi
+
--- src/pages/rosa.js
+++ src/pages/rosa.js
@@ -1,16 +1,112 @@
 import React from 'react';
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
+
+import EventDate from '../components/event-date-time'
 import Layout from "../components/layout"
 import ContentBlock from '../components/content-block'
+import EventListItem from '../components/event-list-item'
 
 class PankeRosa extends React.Component{
+  
+  returnEventListItem = (ev) => {
+    console.log('hä?')
+    return (
+      <EventListItem
+        key={ev.slug}
+        event={ev}
+      />
+    )
+  }
 
   render() {
 
     const blocks = get(this, 'props.data.allContentfulContentBlock.edges');
+    
+    const events = get(this, 'props.data.allContentfulEvent.edges');
+    console.log('Events from query', events)
+    
+    // Filter array of events    
+    function filterUpcoming(_ev) {
+      var currentDate = new Date();
+      var eventDate = new Date(_ev.node.date);
+      return eventDate >= currentDate;
+    }
+    const upcomingEvents = events.filter(filterUpcoming);
+    upcomingEvents.reverse();
+    
+    function filterPast(_ev) {
+      var currentDate = new Date();
+      var eventDate = new Date(_ev.node.date);
+      return eventDate < currentDate;
+    }
+    const pastEvents = events.filter(filterPast);
+    
+    var upcoming;
+    if (upcomingEvents.length > 0){
+      upcoming = (
+        <section className="upcoming">
+          
+          <div className="row headline">
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <h2>Upcoming at /rosa</h2>  
+            </div>
+            
+            <div className="col-md-8 col-sm-8 col-xs-12">
+              {upcomingEvents.map(({ node }) => (
+                <>
+                  <Link to={`/event/${node.slug}`}>
+                    <GatsbyImage alt="FeaturedImage" image={node.featuredImage.gatsbyImageData} aspectratio={16/9} />
+                  </Link>
+                  <h3 className="mt-3">
+                    <Link to={`/event/${node.slug}`}>{node.title}</Link>
+                  </h3>
+                  <div dangerouslySetInnerHTML={{
+                      __html: node.subtitleShortDescription.childMarkdownRemark.html
+                    }} />
+                  <p className="meta mb-5"><EventDate event={node} /></p>
+                </>
+              ))}
+            </div>
+          </div>
+        </section>
+      );
+    }
+
+    var past;
+    if (pastEvents.length > 0) {
+      console.log('past', pastEvents)
+      past = (
+        <section className="past">
+          
+          <div className="row headline">
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <h2>Past events at rosa</h2>
+            </div>
+            
+            <div className="col-md-8 col-sm-8 col-xs-12">
+              {pastEvents.map(({ node }) => (
+                <>
+                  <h3>
+                    <Link to={`/event/${node.slug}`}>{node.title}</Link>
+                  </h3>
+                  <div dangerouslySetInnerHTML={{
+                      __html: node.subtitleShortDescription.childMarkdownRemark.html
+                    }} />
+                  <p c  lassName="meta mb-5"><EventDate event={node} /></p>
+                </>
+              ))}
+            </div>
+          </div>
+        </section>
+      );
+    }
+    
+
+    
 
     let infoBlock = {
       title: 'About',
@@ -21,8 +117,6 @@ class PankeRosa extends React.Component{
       title: 'More',
       content: `/rosa offers a site for local and international dialogues between researchers, students, net art enthusiasts, and established and emerging artists. In addition to showing artworks, /rosa proposes experimental formats of art mediation, with public lectures and a workspace, as well as a growing research library for self-study. It also  functions as a venue for community events such as reading groups and workshops.`
     }
-
-
 
     let address = `
       <strong>/rosa</strong><br>
@@ -47,8 +141,8 @@ class PankeRosa extends React.Component{
         <section className="info">
           <div className="row">
             <div className="col-md-4 col-sm-4 col-xs-12">
-                      <h2>{infoBlock.title}</h2>
-                  </div>
+              <h2>{infoBlock.title}</h2>
+            </div>
             <div className="col-md-8 col-sm-8 col-xs-12">
               {<div dangerouslySetInnerHTML={{
                 __html: infoBlock.content
@@ -77,6 +171,10 @@ class PankeRosa extends React.Component{
               <ContentBlock key={node.id} blockTitle={node.title} blockContent={node.blockContent} />
           )
         })}
+          
+        {upcoming}
+          
+        {past}
 
         </main>
       </Layout>
@@ -99,6 +197,32 @@ export const pageQuery = graphql`
             childMarkdownRemark {
               html
             }
+          }
+        }
+      }
+    }
+    allContentfulEvent(
+      sort: { fields: [date], order: DESC }
+			filter: {eventSeries: {slug: {eq: "rosa"}}}
+    ) {
+      edges {
+        node {
+          title
+          slug
+          date
+          endTime
+          openEnd
+          subtitleShortDescription {
+            childMarkdownRemark {
+              html
+            }
+          }
+          featuredImage {
+            gatsbyImageData(
+              layout: CONSTRAINED,
+              width: 1050,
+              placeholder: BLURRED
+            )
           }
         }
       }
