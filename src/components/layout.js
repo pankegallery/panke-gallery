1dbf0d03f562efeb5a0e58eaa5c21a3b9dd00b80
crosssenses <lari_wu@gmx.de>
2024-08-07 01:49:48 +0200

    temporary minimal site for solar network

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,103 +1,123 @@
-import React, {useState} from 'react'
-import Helmet from 'react-helmet'
+import React, { useState } from "react";
+import Helmet from "react-helmet";
+import ColorSwap from "../components/color-swap";
+import Footer from "../components/footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import "../styles/panke.scss";
+import { ThemeProvider, ThemeContext } from "../contexts/ThemeContext";
+import Header from "./header";
+import Navigation from "./navigation";
 
-import Header from '../components/header'
-import Navigation from '../components/navigation'
-import ColorSwap from '../components/color-swap'
-import Footer from '../components/footer'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-
-import '../styles/panke.scss';
-
-import {ThemeProvider, ThemeContext} from '../contexts/ThemeContext';
-
-const TemplateWrapper = ({ children }) => {
-
+const Layout = ({ children, redirectActive }) => {
   const [offCanvas, setOffCanvas] = useState(false);
 
   const handleClick = () => {
-    setOffCanvas(prevOffCanvas => !prevOffCanvas)
-  }
+    setOffCanvas((prevOffCanvas) => !prevOffCanvas);
+  };
 
-  let offCanvasClass = `off-canvas ${offCanvas ? 'active' : ''}`
+  const offCanvasClass = `off-canvas ${offCanvas ? "active" : ""}`;
 
   return (
     <ThemeProvider>
       <ThemeContext.Consumer>
-        {({color, changeThemeColor}) => (
-          <div
-            className="pageWrapper"
-          >
+        {({ color, changeThemeColor }) => (
+          <div className="pageWrapper">
             <Helmet
-              titleTemplate='%s · panke.gallery'
-              title='Home'
+              titleTemplate="%s · panke.gallery"
+              title="Home"
               meta={[
-                { name: 'description', content: 'panke.gallery seeks to open up a dialogue between established and emerging artists whose work comes out of the connections between digital or net-based art and club culture, especially in the recent history of Berlin. Its program of exhibitions and events takes place in a gallery space within the premises of panke.club.' },
-                { name: 'keywords', content: 'net art, Netzkunst, Galerlie, gallery, Berlin, Wedding, Sakrowski, transmediale, Kultur' },
-                { name: 'author', content: 'panke.gallery – Verein für künstlerisch-kulturelle Bildung e.V.' },
+                {
+                  name: "description",
+                  content:
+                    "panke.gallery seeks to open up a dialogue between established and emerging artists whose work comes out of the connections between digital or net-based art and club culture, especially in the recent history of Berlin. Its program of exhibitions and events takes place in a gallery space within the premises of panke.club.",
+                },
+                {
+                  name: "keywords",
+                  content:
+                    "net art, Netzkunst, Galerlie, gallery, Berlin, Wedding, Sakrowski, transmediale, Kultur",
+                },
+                {
+                  name: "author",
+                  content:
+                    "panke.gallery – Verein für künstlerisch-kulturelle Bildung e.V.",
+                },
               ]}
             />
-
             <Helmet>
               {/* Favicons for all platforms */}
-              <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-              <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-              <link rel="icon" type="image/png" sizes="16x16" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicons/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicons/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicons/favicon-16x16.png"
+              />
               <link rel="manifest" href="/favicons/site.webmanifest" />
-              <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+              <link
+                rel="mask-icon"
+                href="/favicons/safari-pinned-tab.svg"
+                color="#5bbad5"
+              />
               <link rel="shortcut icon" href="/favicons/favicon.ico" />
               <meta name="msapplication-TileColor" content="#2b5797" />
-              <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+              <meta
+                name="msapplication-config"
+                content="/favicons/browserconfig.xml"
+              />
               <meta name="theme-color" content="#ffffff" />
-
-              {/* CDN Scripts for Slideshow, Icons and */}
-              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" type="text/javascript" />
-              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" type="text/javascript" />
-
-              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" type="style/css" />
-{/*
-              {/* panke.gallery scripts for link color
-              <script src="scripts/js.cookie.js" type="text/javascript" />
-              <script src="scripts/panke.js" type="text/javascript" />
-*/}
-
+              {/* CDN Scripts for Slideshow, Icons */}
+              <script
+                src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                type="text/javascript"
+              />
+              <script
+                src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+                type="text/javascript"
+              />
+              <link
+                rel="stylesheet"
+                href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+                type="text/css"
+              />
             </Helmet>
-
-            <div className={offCanvasClass}>
-              <button className="toggle-menu justify-right" onClick={handleClick}>
-                <FontAwesomeIcon icon={faPlus} aria-label="Toggle menu" />
-              </button>
-              <Navigation />
+            {!redirectActive && (
+              <div className={offCanvasClass}>
+                <button
+                  className="toggle-menu justify-right"
+                  onClick={handleClick}
+                >
+                  <FontAwesomeIcon icon={faPlus} aria-label="Toggle menu" />
+                </button>
+                <Navigation />
+              </div>
+            )}
+            <div className={`container theme-${color}`}>
+              {!redirectActive && <Header handleClick={handleClick} />}
+              <main>{children}</main>
+              {!redirectActive && <Footer />}
             </div>
-
-
-                <div className={'container theme-' + color}>
-
-                  <Header handleClick={handleClick} />
-
-                    <main>{children}</main>
-
-                  <Footer />
-
-                </div>
-
-
-            <ColorSwap
-              themeColor={color}
-              changeThemeColor={changeThemeColor}
-            />
-
+            {!redirectActive && (
+              <ColorSwap
+                themeColor={color}
+                changeThemeColor={changeThemeColor}
+              />
+            )}
           </div>
         )}
       </ThemeContext.Consumer>
     </ThemeProvider>
   );
-}
+};
 
-//TemplateWrapper.propTypes = {
-//  children: PropTypes.func,
-//}
-
-export default TemplateWrapper;
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,207 +1,157 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import get from 'lodash/get'
-import Moment from 'moment'
-import { Link, graphql } from 'gatsby'
+import React, { useEffect, useState } from 'react';
+import Helmet from 'react-helmet';
+import get from 'lodash/get';
+import Moment from 'moment';
+import { Link, graphql } from 'gatsby';
 
-import Layout from '../components/layout'
-import ExhibitionPreview from '../components/exhibition-preview'
-import EventPreview from '../components/event-preview'
+import Layout from '../components/layout';
+import ExhibitionPreview from '../components/exhibition-preview';
+import EventPreview from '../components/event-preview';
 
+const PankeIndex = ({ data }) => {
+  const START_DATE = "01/08/2024";
+  const END_DATE = "30/09/2024";
 
-class PankeIndex extends React.Component {
+  const [redirectActive, setRedirectActive] = useState(false);
 
+  useEffect(() => {
+    const currentDate = new Date();
+    const startDate = Moment(START_DATE, 'DD/MM/YYYY').toDate();
+    const endDate = Moment(END_DATE, 'DD/MM/YYYY').toDate();
 
-  filterCurrent = (_ex) => {
-    var currentDate = new Date();
-    var exhibtionStartDate = new Date(_ex.node.startDate);
-    var exhibtionEndDate = new Date(_ex.node.endDate);
-    return Moment(exhibtionStartDate, 'day').utcOffset(120).isSameOrBefore(currentDate, 'day') && Moment(exhibtionEndDate, 'day').utcOffset(120).isSameOrAfter(currentDate, 'day');
-  }
+    console.log("dates", startDate, currentDate, endDate);
+    if (currentDate >= startDate && currentDate <= endDate) {
+      
+      setRedirectActive(true);
+    }
+  }, []);
 
-  filterUpcoming = (_ex) => {
-    var currentDate = new Date();
-    var exhibtionStartDate = new Date(_ex.node.startDate);
-//    return Moment(exhibtionStartDate, 'day').utcOffset(120).isAfter(currentDate, 'day') && !_ex.node.dateTbc;
-    return Moment(exhibtionStartDate, 'day').utcOffset(120).isAfter(currentDate, 'day');
-  }
+  const filterCurrent = (_ex) => {
+    const currentDate = new Date();
+    const exhibitionStartDate = new Date(_ex.node.startDate);
+    const exhibitionEndDate = new Date(_ex.node.endDate);
+    return Moment(exhibitionStartDate).isSameOrBefore(currentDate, 'day') &&
+           Moment(exhibitionEndDate).isSameOrAfter(currentDate, 'day');
+  };
 
-  filterUpcomingEvents = (_ev) => {
-    var currentDate = new Date();
-    var eventStartDate = new Date(_ev.node.date);
-    return Moment(eventStartDate, 'day').utcOffset(120).isSameOrAfter(currentDate, 'day');
-  }
+  const filterUpcoming = (_ex) => {
+    const currentDate = new Date();
+    const exhibitionStartDate = new Date(_ex.node.startDate);
+    return Moment(exhibitionStartDate).isAfter(currentDate, 'day');
+  };
 
-  render() {
+  const filterUpcomingEvents = (_ev) => {
+    const currentDate = new Date();
+    const eventStartDate = new Date(_ev.node.date);
+    return Moment(eventStartDate).isSameOrAfter(currentDate, 'day');
+  };
 
-    console.log('Context: ', get(this.props, 'pageContext'))
-    // Get array of exhibitions
-    const exhibitions = get(this, 'props.data.allContentfulExhibition.edges')
+  const exhibitions = get(data, 'allContentfulExhibition.edges', []);
+  const events = get(data, 'allContentfulEvent.edges', []);
+  const newsItems = get(data, 'allContentfulContentBlock.edges', []);
 
-    // Get array of events
-    const events = get(this, 'props.data.allContentfulEvent.edges')
-    
-    // Get array of news
-    const newsItems = get(this, 'props.data.allContentfulContentBlock.edges')
+  const currentExhibitions = exhibitions.filter(filterCurrent);
+  const upcomingExhibitions = exhibitions.filter(filterUpcoming);
+  const upcomingEvents = events.filter(filterUpcomingEvents).slice(0, 2);
 
-//    console.log("Exhibitions:", posts);
-  //  console.log("Events:", posts);
-//    console.log("news:", newsItems);
-    
-    // Filter array of exhibitions
-    const currentExhibitions = exhibitions.filter(this.filterCurrent);
-    const upcomingExhibitions = exhibitions.filter(this.filterUpcoming);
+  return (
+      <Layout redirectActive={redirectActive}>
+      <Helmet
+        title="Home"
+        meta={[
+          {
+            name: 'description',
+            content: 'News and upcoming exhibitions of panke.gallery in Berlin-Wedding. The gallery seeks to open up a dialogue between established and emerging artists whose work comes out of the connections between digital or net-based art and club culture.'
+          }
+        ]}
+      />
 
-    // Filter array of events and slice to first 2
-    const upcomingEvents = events.filter(this.filterUpcomingEvents).slice(0,2);
-    // Filter by GraphQL (not working, only updated on rebuild)
-//    const upcomingEvents = events
-
-    // Log exhibitions
-//    console.log("Current Exhibitions:", currentExhibitions);
-//    console.log("Upcoming Exhibitions:", upcomingExhibitions);
-   console.log("Upcoming Events:", upcomingEvents);
-
-    // Create news code
-    var news;
-    if (newsItems.length > 0){
-      news = (
+      {newsItems.length > 0 && (
         <section className="news">
-          {newsItems.map(({node}) => {
-            return (
-              <article key={node.id} className="news-item">
-                <div className="row headline">
-                  <div className="col-md-12 col-sm-12 col-xs-12">
-                    <h2>{node.title}</h2>
-                  </div>
+          {newsItems.map(({ node }) => (
+            <article key={node.id} className="news-item">
+              <div className="row headline">
+                <div className="col-md-12 col-sm-12 col-xs-12">
+                  <h2>{node.title}</h2>
                 </div>
-                <div className="row">
-                  {<div className="col-md-12 col-sm-12 col-xs-12" dangerouslySetInnerHTML={{
-                      __html: node.blockContent.childMarkdownRemark.html
-                    }} />}
-                </div>
-              </article>
-            )
-          })}
-
+              </div>
+              <div className="row">
+                <div className="col-md-12 col-sm-12 col-xs-12" dangerouslySetInnerHTML={{ __html: node.blockContent.childMarkdownRemark.html }} />
+              </div>
+            </article>
+          ))}
         </section>
-      );
-    }
+      )}
 
-    // Create upcoming events code if there are
-    var upcomingEv;
-    if (upcomingEvents.length > 0){
-      upcomingEv = (
-        <section className="upcoming">
-
-          <div className="row headline">
-            <div className="col-md-8 col-sm-8 col-xs-12">
-              <h2>Upcoming events</h2>
-            </div>
-            <div className="col-md-4 col-sm-4 col-xs-12 text-sm-right d-none d-sm-block pt-4">
-              <Link to={'events'}><button className="eventSeries">See all events</button></Link>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-12 col-sm-8 col-xs-12">
-              {upcomingEvents.map(({ node }) => {
-                return (
+      {!redirectActive && (
+        <>
+          {upcomingEvents.length > 0 && (
+            <section className="upcoming">
+              <div className="row headline">
+                <div className="col-md-8 col-sm-8 col-xs-12">
+                  <h2>Upcoming events</h2>
+                </div>
+                <div className="col-md-4 col-sm-4 col-xs-12 text-sm-right d-none d-sm-block pt-4">
+                  <Link to={'/events'}><button className="eventSeries">See all events</button></Link>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12 col-sm-8 col-xs-12">
+                  {upcomingEvents.map(({ node }) => (
                     <EventPreview key={node.id} event={node} />
-                )
-              })}
-            </div>
-          </div>
-        </section>
-      );
-    }
+                  ))}
+                </div>
+              </div>
+            </section>
+          )}
 
-    // Create current exhibitions code if there are
-    var current;
-    if (currentExhibitions.length > 0){
-      current = (
-        <section className="currently">
-
-          <div className="row headline">
-            <div className="col-md-12 col-sm-12 col-xs-12">
-              <h2>Current exhibitions</h2>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-12 col-sm-8 col-xs-12">
-              {currentExhibitions.map(({ node }) => {
-                return (
+          {currentExhibitions.length > 0 && (
+            <section className="currently">
+              <div className="row headline">
+                <div className="col-md-12 col-sm-12 col-xs-12">
+                  <h2>Current exhibitions</h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12 col-sm-8 col-xs-12">
+                  {currentExhibitions.map(({ node }) => (
                     <ExhibitionPreview key={node.slug} exhibition={node} />
-                )
-              })}
-            </div>
-          </div>
-        </section>
-      );
-    }
+                  ))}
+                </div>
+              </div>
+            </section>
+          )}
 
-    // Create upcoming exhibitions code if there are
-    var upcoming;
-    if (upcomingExhibitions.length > 0){
-      upcoming = (
-        <section className="upcoming">
-
-          <div className="row headline">
-            <div className="col-md-12 col-sm-12 col-xs-12">
-              <h2>Upcoming exhibitions</h2>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-12 col-sm-8 col-xs-12">
-              {upcomingExhibitions.map(({ node }) => {
-                return (
+          {upcomingExhibitions.length > 0 && (
+            <section className="upcoming">
+              <div className="row headline">
+                <div className="col-md-12 col-sm-12 col-xs-12">
+                  <h2>Upcoming exhibitions</h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12 col-sm-8 col-xs-12">
+                  {upcomingExhibitions.map(({ node }) => (
                     <ExhibitionPreview key={node.slug} exhibition={node} />
-                )
-              })}
-            </div>
-          </div>
-        </section>
-      );
-    }
+                  ))}
+                </div>
+              </div>
+            </section>
+          )}
+        </>
+      )}
+    </Layout>
+  );
+};
 
-    return (
-      <Layout>
-        <Helmet
-          title= '  Home'
-          meta={[
-            {
-              name: 'description',
-              content: 'News and upcoming exhibitions of panke.gallery in Berlin-Wedding. The gallery seeks to open up a dialogue between established and emerging artists whose work comes out of the connections between digital or net-based art and club culture.'
-            }
-          ]}
-        />
-
-        {news}
-
-        {upcomingEv}
-
-        {current}
-
-        {upcoming}
-
-      </Layout>
-
-    );
-  }
-}
-
-export default PankeIndex
+export default PankeIndex;
 
 export const pageQuery = graphql`
   query PankeHomeQuery($today: Date!) {
     allContentfulExhibition(
       sort: { fields: [startDate], order: ASC },
-      # limit: 3,
-      filter: {endDate: {gte: $today}}
-      )
-     {
+      filter: { endDate: { gte: $today } }
+    ) {
       edges {
         node {
           title
@@ -226,10 +176,9 @@ export const pageQuery = graphql`
       }
     }
     allContentfulEvent(
-        sort: { fields: [date], order: ASC },
-        # limit: 2,
-        filter: {date: {gte: $today}}
-      ){
+      sort: { fields: [date], order: ASC },
+      filter: { date: { gte: $today } }
+    ) {
       edges {
         node {
           title
@@ -246,17 +195,17 @@ export const pageQuery = graphql`
             slug
           }
           subtitleShortDescription {
-            childMarkdownRemark{
+            childMarkdownRemark {
               html
             }
           }
         }
       }
     }
-    allContentfulContentBlock (
-        filter: {page: {eq: "News"}},
-        sort: { fields: [updatedAt], order: DESC}
-      ){
+    allContentfulContentBlock(
+      filter: { page: { eq: "News" } },
+      sort: { fields: [updatedAt], order: DESC }
+    ) {
       edges {
         node {
           id
@@ -272,4 +221,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
